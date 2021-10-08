efd89986971e49d80c5b11f525ab7ff810d69f64
Heriyanto <heriyanto.liu@gmail.com>
2021-10-08 17:32:46 +0700

    exercise 3.8

--- part3/phonebook-BE/index.js
+++ part3/phonebook-BE/index.js
@@ -28,7 +28,15 @@ let persons = [
 
 app.use(express.json());
 
-app.use(morgan("tiny"));
+morgan.token("content", (request, response) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :content"
+  )
+);
 
 app.get("/info", (request, response) => {
   response.send(
