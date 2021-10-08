c3ec40f3f7fa648fd4569ce0cd7cbf1f88b7d7ad
Heriyanto <heriyanto.liu@gmail.com>
2021-10-08 17:12:06 +0700

    exercise 3.2

--- part3/phonebook-BE/index.js
+++ part3/phonebook-BE/index.js
@@ -24,6 +24,15 @@ let persons = [
   },
 ];
 
+app.get("/info", (request, response) => {
+  response.send(
+    `Phonebook has info for ${
+      persons.length
+    } people <br/> ${new Date().toLocaleString()}`
+  );
+  response.send(`A`);
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
