2977e0d66d720092481afd20c11e9dce6bd9a265
Heriyanto <heriyanto.liu@gmail.com>
2021-10-11 15:02:20 +0700

    exercise 4.4

--- part4/blog-BE/utils/list_helper.js
+++ part4/blog-BE/utils/list_helper.js
@@ -1,5 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
