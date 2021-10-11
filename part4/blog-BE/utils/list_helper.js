66b65f797a073d9bb5d6d52676ed1a00bd4166ba
Heriyanto <heriyanto.liu@gmail.com>
2021-10-11 15:07:58 +0700

    exercise 4.5

--- part4/blog-BE/utils/list_helper.js
+++ part4/blog-BE/utils/list_helper.js
@@ -7,4 +7,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  let mostLikes = 0
+  blogs.forEach((blog) => {
+    if (blog.likes > mostLikes) {
+      mostLikes = blog.likes
+    }
+  })
+  const favoriteBlog = blogs.find((blog) => blog.likes === mostLikes)
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes,
+  }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
