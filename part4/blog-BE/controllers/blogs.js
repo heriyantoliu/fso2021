8e17b354d18e3c8d2d3aea4712cd21c90ae7883a
Heriyanto <heriyanto.liu@gmail.com>
2021-10-12 13:16:00 +0700

    exercise 4.21

--- part4/blog-BE/controllers/blogs.js
+++ part4/blog-BE/controllers/blogs.js
@@ -40,6 +40,18 @@ blogsRouter.put('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, config.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog.user.toString() !== decodedToken.id) {
+    return response
+      .status(401)
+      .json({ error: 'cannot delete another user blog' })
+  }
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
