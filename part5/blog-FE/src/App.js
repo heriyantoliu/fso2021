dd11a239ef0e44efc76d2a588c56e7e30357756e
Heriyanto <heriyanto.liu@gmail.com>
2021-10-12 16:41:47 +0700

    exercise 5.9

--- part5/blog-FE/src/App.js
+++ part5/blog-FE/src/App.js
@@ -110,9 +110,11 @@ const App = () => {
       <Togglable buttonLabel="create new blog" ref={blogFormRef}>
         <BlogForm newBlog={handleNewBlog} />
       </Togglable>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} addLikes={handleAddLikes} />
-      ))}
+      {blogs
+        .sort((a, b) => a.likes - b.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} addLikes={handleAddLikes} />
+        ))}
     </div>
   );
 
