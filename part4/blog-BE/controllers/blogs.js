8d9cfd79be2024b8ef7b30ff2f6d21829ad9e844
Heriyanto <heriyanto.liu@gmail.com>
2021-10-12 13:05:27 +0700

    exercise 4.19

--- part4/blog-BE/controllers/blogs.js
+++ part4/blog-BE/controllers/blogs.js
@@ -1,6 +1,18 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const config = require('../utils/config')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,17 +21,20 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  try {
-    const result = await blog.save()
 
-    const user = await User.findById(blog.user)
-    user.blogs = user.blogs.concat(result._id)
-    await User.findByIdAndUpdate(user.id, user)
-
-    response.status(201).json(result)
-  } catch (exception) {
-    response.status(400).send({ error: 'invalid' })
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, config.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
   }
+
+  const user = await User.findById(decodedToken.id)
+
+  const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await User.findByIdAndUpdate(user.id, user)
+
+  response.status(201).json(result)
 })
 
 blogsRouter.put('/:id', async (request, response) => {
