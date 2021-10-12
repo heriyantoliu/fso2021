cbbad0eb8d0f25e21e1e9322bf31d33afff4f05a
Heriyanto <heriyanto.liu@gmail.com>
2021-10-12 11:59:06 +0700

    exercise 4.16

--- part4/blog-BE/controllers/users.js
+++ part4/blog-BE/controllers/users.js
@@ -9,6 +9,13 @@ usersRouter.get('/', async (request, response) => {
 
 usersRouter.post('/', async (request, response) => {
   const body = request.body
+
+  if (body.password.length < 3) {
+    response.status(400).json({
+      error: 'min password length is 3 character',
+    })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
