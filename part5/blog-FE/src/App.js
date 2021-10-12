8ca8faec0c464d6c58ec426b9581bc42b5348521
Heriyanto <heriyanto.liu@gmail.com>
2021-10-12 14:51:28 +0700

    exercise 5.2

--- part5/blog-FE/src/App.js
+++ part5/blog-FE/src/App.js
@@ -13,14 +13,31 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggerUserJSON = window.localStorage.getItem('loggedBlogappUser');
+    if (loggerUserJSON) {
+      const user = JSON.parse(loggerUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  });
+
   const handleLogin = async (event) => {
     event.preventDefault();
 
     const user = await loginService.login({ username, password });
 
+    window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
+    blogService.setToken(user.token);
     setUser(user);
     setUsername('');
     setPassword('');
+  };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser');
+    blogService.setToken('');
+    setUser(null);
   };
 
   const loginForm = () => (
@@ -50,7 +67,9 @@ const App = () => {
   const blogsList = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in<button onClick={handleLogout}>logout</button>
+      </p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
