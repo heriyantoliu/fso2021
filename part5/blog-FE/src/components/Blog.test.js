aecc4073c3dc11ff5ca2c1a074463eb0a0c02be4
Heriyanto <heriyanto.liu@gmail.com>
2021-10-13 09:52:54 +0700

    exercise 5.14

--- part5/blog-FE/src/components/Blog.test.js
+++ part5/blog-FE/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('render Note', () => {
@@ -11,10 +11,18 @@ test('render Note', () => {
     likes: 2,
   }
 
+  // const mockHandler = jest.fn()
+
   const component = render(<Blog blog={blog} />)
 
   expect(component.container).toHaveTextContent('Title 1 Author 1')
 
   const div = component.container.querySelector('.togglableContent')
   expect(div).toHaveStyle('display: none')
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  const div2 = component.container.querySelector('.togglableContent')
+  expect(div2).not.toHaveStyle('display: none')
 })
