689e2000f33908cca691ebac302fa24e914920b0
Heriyanto <heriyanto.liu@gmail.com>
2021-10-13 13:09:18 +0700

    exercise 5.18

--- part5/blog-FE/cypress/integration/blog_app.spec.js
+++ part5/blog-FE/cypress/integration/blog_app.spec.js
@@ -18,4 +18,23 @@ describe('Blog app', () => {
   it('Login form is show', () => {
     cy.contains('login').click()
   })
+
+  describe('Login', () => {
+    it('succeeds with correct credentials', () => {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', () => {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+  })
 })
--- part5/blog-FE/src/App.js
+++ part5/blog-FE/src/App.js
@@ -102,7 +102,9 @@ const App = () => {
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id="login-button" type="submit">
+        login
+      </button>
     </form>
   )
 
