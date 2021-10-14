5423a3f9334c4bf133f9d7407c5a4c7959206639
Heriyanto <heriyanto.liu@gmail.com>
2021-10-14 11:38:53 +0700

    exercise 7.5

--- part7/anecdotes-routed/src/App.js
+++ part7/anecdotes-routed/src/App.js
@@ -112,6 +112,12 @@ const CreateNew = (props) => {
     history.push('/');
   };
 
+  const handleReset = () => {
+    content.reset();
+    author.reset();
+    info.reset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -128,7 +134,10 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
       </form>
     </div>
   );
