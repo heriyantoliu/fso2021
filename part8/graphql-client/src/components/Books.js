ba005d6488f88fd35603238c4fe92e64e340893d
Heriyanto <heriyanto.liu@gmail.com>
2021-10-17 15:17:57 +0700

    exercise 8.19

--- part8/graphql-client/src/components/Books.js
+++ part8/graphql-client/src/components/Books.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { ALL_BOOKS } from '../queries';
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS);
+  const [genre, setGenre] = useState('All');
 
   if (!props.show) {
     return null;
   }
 
   const books = [...result.data.allBooks];
+  let allGenres = [];
+  books.forEach((book) => {
+    allGenres = allGenres.concat(book.genres);
+  });
+
+  const uniqueGenres = allGenres.filter((genre, index, self) => {
+    return self.indexOf(genre) === index;
+  });
+
+  const filteredBooks =
+    genre === 'All'
+      ? books
+      : books.filter((book) => book.genres.includes(genre));
 
   return (
     <div>
       <h2>books</h2>
-
+      <div>
+        in genre <b>{genre}</b>
+      </div>
       <table>
         <tbody>
           <tr>
@@ -22,7 +38,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -31,6 +47,12 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {uniqueGenres.map((genre) => (
+        <button key={genre} onClick={() => setGenre(genre)}>
+          {genre}
+        </button>
+      ))}
+      <button onClick={() => setGenre('All')}>all genres</button>
     </div>
   );
 };
