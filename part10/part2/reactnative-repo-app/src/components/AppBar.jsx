094c7b1614c13fd3bafb8f4fae6ea6c649f2289e
Heriyanto <heriyanto.liu@gmail.com>
2021-10-24 20:40:58 +0700

    exercise 10.7

--- part10/part2/reactnative-repo-app/src/components/AppBar.jsx
+++ part10/part2/reactnative-repo-app/src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../../theme';
@@ -15,14 +15,12 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
-  const repoPressed = () => {
-    Alert.alert('clicked');
-  };
-
   return (
     <View style={styles.container}>
-      <AppBarTab text="Repositories" link="/" />
-      <AppBarTab text="Sign In" link="/signin" />
+      <ScrollView horizontal>
+        <AppBarTab text="Repositories" link="/" />
+        <AppBarTab text="Sign In" link="/signin" />
+      </ScrollView>
     </View>
   );
 };
