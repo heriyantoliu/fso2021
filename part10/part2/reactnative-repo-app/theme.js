559660c894da293ff69a297cb6c5c5fee5714ed3
Heriyanto <heriyanto.liu@gmail.com>
2021-10-24 21:33:51 +0700

    exercise 10.10

--- part10/part2/reactnative-repo-app/theme.js
+++ part10/part2/reactnative-repo-app/theme.js
@@ -1,8 +1,17 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     appBarBackgroundColor: '#24292e',
     appBarTextColor: 'white',
     textSecondary: '#586069',
+  },
+  fonts: {
+    main: Platform.select({
+      default: 'san-serif',
+      android: 'roboto',
+      ios: 'arial',
+    }),
   },
 };
 
