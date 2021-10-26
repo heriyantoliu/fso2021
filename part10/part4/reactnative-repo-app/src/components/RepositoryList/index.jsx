8d51a29edeeb37b4215a1476e9996d808ff23c0a
Heriyanto <heriyanto.liu@gmail.com>
2021-10-26 13:16:38 +0700

    exercise 10.23

--- part10/part4/reactnative-repo-app/src/components/RepositoryList/index.jsx
+++ part10/part4/reactnative-repo-app/src/components/RepositoryList/index.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
+import { StyleSheet } from 'react-native';
 import RepositoryListContainer from './RepositoryListContainer';
 import useRepositories from '../../hooks/useRepositories';
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
+const styles = StyleSheet.create({
+  picker: {
+    margin: 5,
+    fontSize: 16,
+  },
+});
 
-  return <RepositoryListContainer repositories={repositories} />;
+const RepositoryList = () => {
+  const [selected, setSelected] = useState('latestRepo');
+  const { repositories } = useRepositories(selected);
+
+  return (
+    <>
+      <Picker
+        style={styles.picker}
+        selectedValue={selected}
+        onValueChange={(itemValue, itemIndex) => {
+          setSelected(itemValue);
+        }}
+      >
+        <Picker.Item label="Latest Repositories" value="latestRepo" />
+        <Picker.Item label="Highest Rate Repositories" value="highRateRepo" />
+        <Picker.Item label="Lowest Rate Repositories" value="lowRateRepo" />
+      </Picker>
+      <RepositoryListContainer repositories={repositories} />
+    </>
+  );
 };
 
 export default RepositoryList;
--- part10/part4/reactnative-repo-app/src/hooks/useRepositories.js
+++ part10/part4/reactnative-repo-app/src/hooks/useRepositories.js
@@ -2,9 +2,35 @@ import { useQuery } from '@apollo/client';
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const useRepositories = (orderBy) => {
+  let queryVariables = {};
+
+  switch (orderBy) {
+    case 'latestRepo':
+      queryVariables = {
+        orderBy: 'CREATED_AT',
+        orderDirection: 'DESC',
+      };
+      break;
+    case 'highRateRepo':
+      queryVariables = {
+        orderBy: 'RATING_AVERAGE',
+        orderDirection: 'DESC',
+      };
+      break;
+    case 'lowRateRepo':
+      queryVariables = {
+        orderBy: 'RATING_AVERAGE',
+        orderDirection: 'ASC',
+      };
+      break;
+    default:
+      break;
+  }
+
   const { data, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
+    variables: queryVariables,
   });
 
   const repositories = loading ? undefined : data.repositories;
